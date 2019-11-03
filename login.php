a7326ed278f3595010278f0e792cc0b36e305065
Farazahmed90 <khatrifaraz40@gmail.com>
2019-11-03 14:55:01 +0500

    first commit

---
+++ login.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+<body background="images/Untitled-1.jpg">
+<body>
+<table width="1334">
+<thead>
+
+<tr>
+  <td width="386"><a href="index.html"><img src="images/logo-new-landmark.png" width="285" height="113" alt="logo" longdesc="http://index.html"></a></td> 
+  <td width="135" align="center"><b><a href="index.html">HOME</a></b></td>
+  <td width="122" align="center"><b><a href="login.php">LOGIN</a></b></td><td width="122"><b><a href="about us.html" target="new">ABOUT US</a></b></td><td width="129"><b><a href="feed back.html" target="new">FEED BACK</a></b><a href="feed back.html"></a></a></td><td width="152"><b><a href="join us.html"><a href="join us.html"><b>APPLY NOW</a></b></td>
+  <td width="282"><b><a href="contact us.html" target="new">CONTACT US</a></b></td></tr>
+</thead>
+</table>
+
+<form action="welcome administrator.php" method="post" >
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<table align="center">
+<tr>
+<td>User Name :</td>
+<td><input type="text" placeholder="Enter your username.. " name="username"/></td>
+</tr>
+
+<tr>
+<td>Password :</td>
+<td><input type="text" placeholder="Enter your password.. " name="password"/></td>
+</tr>
+
+
+
+<tr>
+
+<td colspan="2" align="center"><input type="submit" value="LOGIN" />
+</tr>
+
+
+
+</table>
+
+
+
+
+</form>
+</body>
+</html>
---
+++ welcome administrator.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>LAND MARK</title>
+</head>
+<body background="Untitled-1.jpg" style="background-attachment:fixed";>
+<table width="1334">
+<thead>
+
+<tr>
+  <td width="386"><a href="index.html"><img src="logo-new-landmark.png" width="285" height="113" alt="logo" longdesc="http://index.html"></a></td> 
+  <td width="135" align="center"><b><a href="index.html">HOME</a></b></td>
+  <td width="122" align="center"><b><a href="login.php">LOGIN</a></b></td><td width="122"><b><a href="about us.html" target="new">ABOUT US</a></b></td><td width="129"><b><a href="feed back.html" target="new">FEED BACK</a></b><a href="feed back.html"></a></a></td><td width="152"><b><a href="join us.html"><a href="join us.html"><b>APPLY NOW</a></b></td>
+  <td width="282"><b><a href="contact us.html" target="new">CONTACT US</a></b></td></tr>
+</thead>
+
+
+
+</table>
+
+<table width="1112" border="0">
+  <tr>
+    <th width="246" height="63" scope="col">&nbsp;</th>
+    <th width="800" scope="col"><b>WELCOME ADMINISTRATOR <br>PROJECTS AVALIABLE</b></th>
+    <th width="52" scope="col">&nbsp;</th>
+  </tr>
+</table>
+<table width="1282" height="174" border="0">
+  <tr>
+    <td width="169"></td>
+    <td width="335"><a href="current project.html" target="new"><img src="bahria-town-logo-D1A3F8C43C-seeklogo.com.png" width="208" height="227" alt="Bahri Town"></a></td>
+    <td width="300"><a href="kings project.html" target="new"><img src="Logo-of-kings-Group.png" width="252" height="300" alt="Kings logo"></a></td>
+    <td width="460"><a href="saima project.html" target="new"><img src="saima-Group.png" width="326" height="305" alt="Saim Group"></a></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+</body>
+</html
+><?php 
+if ($_GET['username']==='abc' && $_GET['password']==='123' )
+{
+echo "<center><b>Your name is ".$_GET['username']."<br>";
+echo "<center><b>Your password is ".$_GET['password']."<br>";
+
+}
+else {
+	echo "<center>login failed";
+	
+	
+	}
+?>
+
+<html>
+<body>
+<form action="login.php" method="GET">
+<input type="submit" value="Go back" />
+
+
+<body>
+</html>
